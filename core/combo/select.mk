MWisBest/android_build

Squashed commit of FML changes.
Optimize, optimize, optimize...

Prepare GCC 4.8 support

Suppress new warnings, enable GCC colors.

Optimizing, small fixes, etc.

+-

Automatically set these vars on envsetup.

build: Don't work around cortex-a8 issues when targeting a9 only

Don't work around cortex-a8 issues when other compiler flags
show we want to target cortex-a9 only

Change-Id: If5a077243fc15298fbcbe2ffc857b5e961dd29da
Signed-off-by: Bernhard Rosenkraenzer <Bernhard.Rosenkranzer@linaro.org>

build: Add support for TARGET_EXTRA_C{,PP}FLAGS

Add support for setting TARGET_EXTRA_C{,PP}FLAGS in board configs.
This can be used to set compiler flags for a board that break other
boards, such as -march=cortex-a9 or -DUSE_CUSTOM_EXTENSIONS_FOR_XYZ

Change-Id: I0efd4e37578bf6d239bf56dfeeaaed44840ee3af
Signed-off-by: Bernhard Rosenkraenzer <Bernhard.Rosenkranzer@linaro.org>

build/core: Fix build errors

- Include task hooks only if they exists
- Fix typos

Signed-off-by: Amit Pundir <amit.pundir@linaro.org>

Conflicts:
	core/legacy_prebuilts.mk

Add support for instruction set features

This adds the --instruction-set-features= arguments to the dex2oat
commands for the system images.  The feature set is determined at
build time

(cherry picked from commit 4145a2a20a0f275fe4c77e39d507cd5f8eb7a73b)

(cherry picked from commit 5174bb052986e9d90b597824a7e7b9ee2dfc8668)

Change-Id: I45af3b59752a48897bff2dd468b74f3ea73000be

core/combo/select.mk
@@ -47,8 +47,13 @@ $(combo_target)HAVE_STRLCAT := 0
 $(combo_target)HAVE_KERNEL_MODULES := 0
 
 $(combo_target)GLOBAL_CFLAGS := -fno-exceptions -Wno-multichar
-$(combo_target)RELEASE_CFLAGS := -O2 -g -fno-strict-aliasing
-$(combo_target)GLOBAL_LDFLAGS :=
+$(combo_target)RELEASE_CFLAGS := -O3 -g -Wstrict-aliasing=2
+ifneq ($(combo_target),HOST_)
+$(combo_target)RELEASE_CFLAGS += -Werror=strict-aliasing
+else
+$(combo_target)RELEASE_CFLAGS += -Wno-error=strict-aliasing
+endif
+$(combo_target)GLOBAL_LDFLAGS := -Wl,-O2
 $(combo_target)GLOBAL_ARFLAGS := crsP
 
 $(combo_target)EXECUTABLE_SUFFIX :=

MWisBest/build

	core/combo/TARGET_linux-arm.mk
	core/combo/select.mk
	core/dex_preopt.mk
	core/llvm_config.mk
	envsetup.sh

core/combo/select.mk
@@ -54,9 +54,14 @@ $(combo_var_prefix)HAVE_STRLCAT := 0
 $(combo_var_prefix)HAVE_KERNEL_MODULES := 0
 
 $(combo_var_prefix)GLOBAL_CFLAGS := -fno-exceptions -Wno-multichar
-$(combo_var_prefix)RELEASE_CFLAGS := -O2 -g -fno-strict-aliasing
+$(combo_var_prefix)RELEASE_CFLAGS := -O3 -g -Wstrict-aliasing=2
+ifneq ($(combo_var_prefix),HOST_)
+$(combo_var_prefix)RELEASE_CFLAGS += -Werror=strict-aliasing
+else
+$(combo_var_prefix)RELEASE_CFLAGS += -Wno-error=strict-aliasing
+endif
 $(combo_var_prefix)GLOBAL_CPPFLAGS :=
-$(combo_var_prefix)GLOBAL_LDFLAGS :=
+$(combo_var_prefix)GLOBAL_LDFLAGS := -Wl,-O2
 $(combo_var_prefix)GLOBAL_ARFLAGS := crsPD
 $(combo_var_prefix)GLOBAL_LD_DIRS :=
 